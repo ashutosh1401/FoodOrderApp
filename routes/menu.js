35e980b2c43987f4038d072b01907b288bf4cf36
unknown <ashutoshthakur_14@yahoo.com>
2021-01-29 23:22:06 +0530

    Added Delete Route and working on Update Model

--- routes/menu.js
+++ routes/menu.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Menu = require("../models/menu");
 const { authOwner } = require("../middleware/authenticate");
 
-router.post("/owner/createmenuitem", authOwner, (req, res) => {
+router.post("/menu/createmenuitem", authOwner, (req, res) => {
   const { ItemName, ItemDesc, ItemCost, ItemPhoto } = req.body;
 
   const MenuItem = new Menu({
@@ -23,18 +23,57 @@ router.post("/owner/createmenuitem", authOwner, (req, res) => {
     });
 });
 
-router.get("/owner/viewitem", authOwner, (req, res) => {
-  console.log(req);
+router.get("/menu/viewitem", authOwner, (req, res) => {
   Menu.find({ postedBy: req.user._id })
-    .populate("postedBy", "_id ItemName ItemDesc ItemCost")
+    .populate("postedBy", "_id ItemName ItemDesc ItemCost ItemPhoto")
     .then((result) => {
       res.json({ result });
-      console.log(result);
+      //console.log(result);
     })
     .catch((err) => {
       res.status(404).send({ err });
-      console.log(err);
+      //console.log(err);
     });
+});
+
+router.delete("/menu/:id", authOwner, (req, res) => {
+  //console.log(req.params);
+  Menu.findOne({ _id: req.params.id })
+    .populate("postedBy", "_id ItemName ItemDesc ItemCost ItemPhoto")
+    .exec((err, menu) => {
+      if (err || !menu) {
+        return res.status(422).json({ error: err });
+      }
+      if (menu.postedBy._id.toString() === req.user._id.toString()) {
+        menu
+          .remove()
+          .then((result) => {
+            res.json(result);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+});
+
+router.patch("/menu/:id", authOwner, (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["ItemName", "ItemDesc", "ItemCost"];
+  const isAllowedUpdate = updates.forEach((update) => {
+    allowedUpdates.includes(update);
+  });
+  console.log(updates, isAllowedUpdate);
+  if (!isAllowedUpdate) {
+    return res.status(400).send({ error: "Invalid Update" });
+  }
+
+  Menu.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $push: {},
+    }
+  );
 });
 
 module.exports = router;
