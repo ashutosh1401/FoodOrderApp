a2caf64474919c1cdd927076f1fa7cd25bf39024
Ashutosh Thakur <ashutoshthakur_14@yahoo.com>
2022-01-11 23:53:38 +0530

    Updated components in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from './pages/Home/Home'
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Home />
+      {/* <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/faq" component={Faq} />
+      </Switch> */}
+    </Router>
   );
 }
-
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,56 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+  },
+ logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "17px",
+    marginLeft: theme.spacing(3),
+    "&:hover": {
+      color: "yellow",
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  },
 }));
 
-export default function Navbar() {
-    const classes = useStyles();
+function Navbar() {
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Fooddy...
-          </Typography>
-                    <Button color="inherit">Login</Button>
-                    <Button color="inherit">Signup</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  return (
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" className={classes.logo}>
+          Navbar
+        </Typography>
+          <div className={classes.navlinks}>
+            <Link to="/about" className={classes.link}>
+              About
+            </Link>
+            <Link to="/contact" className={classes.link}>
+              Contact
+            </Link>
+            <Link to="/faq" className={classes.link}>
+              FAQ
+            </Link>
+          </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
+export default Navbar;
