5b2af4deb6ce14320152a13c599f1a62cfd88166
Ashutosh Thakur <ashutoshthakur_14@yahoo.com>
2022-01-11 23:52:09 +0530

    Addded Feature components

---
+++ client/src/components/FeatureCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+      borderRadius: "5%",
+      '&:hover': {
+          transform: "scale(1.1)"
+      }
+    },
+    media: {
+      height: 180,
+    },
+    heading: {
+        textAlign: "center"
+    }
+  });
+  
+
+function FeatureCard({heading,image}) {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2" className={classes.heading}>
+            {heading}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    )
+}
+
+export default FeatureCard
