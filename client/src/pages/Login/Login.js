acdc1322126bd7f4cc16831a7dda15db9ca2bba1
Ashutosh Thakur <ashutoshthakur_14@yahoo.com>
2022-01-15 23:59:57 +0530

    Added Login Page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,22 +1,26 @@
 import React,{useState} from 'react'
-import { makeStyles } from '@material-ui/core';
+import { Link, makeStyles, Typography } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { Grid, Paper, Avatar } from '@material-ui/core';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: theme.spacing(2),
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '300px',
-          },
-          '& .MuiButtonBase-root': {
-            margin: theme.spacing(2),
-          },
+    paperStyle: {
+        padding :20,
+        height:'60vh',
+        width:310, 
+        margin:"20px auto"
+    },
+    textInput: {
+        margin: "5px auto",
+        padding: '2px',
+    },
+    btnstyle:{
+        margin:'14px 0'
+    },
+    avatarStyle: {
+        backgroundColor: "#ff5533",
     }
 }))
 
@@ -25,14 +29,20 @@ function Login() {
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
     return (
-        <div>
-            <form className={classes.root}>
+        <Grid>
+        <Paper elevation={10} className={classes.paperStyle}>
+            <Grid align='center'>
+                <Avatar className={classes.avatarStyle}><FastfoodIcon /></Avatar>
+                <h2>Log In</h2>
+            </Grid>
                 <TextField  
                     label="Email" 
                     variant="filled"
                     type="email"
                     required
                     values={email}
+                    fullWidth
+                    className={classes.textInput}
                     onChange={(e) => setEmail(e.target.value)}
                     />
                 <TextField 
@@ -41,15 +51,23 @@ function Login() {
                     type="password"
                     required
                     values={password}
+                    fullWidth
+                    className={classes.textInput}
                     onChange={(e) => setPassword(e.target.value)}
                     />
-                <div>
-                    <Button type="submit" variant="contained" color="primary">
-                    Login
-                    </Button>
-                </div>
-            </form>
-        </div>
+                    <Button type='submit' color='primary' variant="contained" className={classes.btnstyle} fullWidth>Sign in</Button>
+                <Typography>
+                    <Link to="#">
+                        Forgot password ?
+                    </Link>
+                </Typography>
+                <Typography > Don't you have an account ?
+                     <Link href="/">
+                        Sign Up 
+                    </Link>
+                </Typography>
+            </Paper>
+        </Grid>
     )
 }
 
